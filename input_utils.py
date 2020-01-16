087cc573169d2fe5b78a21cd309abc5280b0d46b
gshachar12 <57983567+gshachar12@users.noreply.github.com>
2020-01-16 14:57:33 +0200

    Add files via upload
    version 1-will not be used now on account of memory problems

--- input_utils.py
+++ input_utils.py
@@ -1,41 +1,15 @@
-
-""" data representation of the input- converts the musical input into a machine readable language"""
-
-import numpy as np
-normalize_val = 100.0
-
-
-""" parameters and initialization"""
-n_h = 90  # the number of hidden states
-n_x = 88  # the number of notes in the piano, the number of unique possible notes in the database
-T_x = 24  # the number of time steps
-
-def input2matrix(pitches, velocities, time_stamps, notes_flag):
+def one_hot(vocabulary_size, input_val):
     """
+    :
+    build a one hot vector from the given input
 
-    :param pitches:
-    :param velocities:
-    :param time_stamps:
+    :param:
+     vocabulary_size: the number of unique characters in the database
+     input_val: the index of the value in the list of unique characters
     :return:
     """
-    T_x=time_stamps[-1]+1 # number of ticks
-    n_x=128 # number of pitches in a vector (there are 128 possible pitches in a midi file)
+    one_hot=np.zeros((vocabulary_size, 1)) # a new array of zeros
+    one_hot[input]=1
 
-    #build a one hot vector from the given input
-    notes_vector = np.zeros((n_x))  # a new array of zeros that will be used to keep track of the notes being played
-
-    matrix=np.zeros((n_x, T_x))
-    for t in range(len(time_stamps)):
-        pitch=pitches[t] # the current pitch being played
-
-        velocity=velocities[t] # the current velocity of the pitch
-        if notes_flag[t]: # a note is being played
-            notes_vector[pitch]=velocity
-        else: # note off event
-            notes_vector[pitch] = 0
-        matrix[:, time_stamps[t]] = notes_vector
-
-    return matrix
-def fit3d(input):
-    x=np.array(n_x, m, T_x)
+    return one_hot
 
--- midi.py
+++ midi.py
@@ -1,95 +1,28 @@
 import py_midicsv
 import numpy as np
-import input_utils
 
-from mido import Message, MidiFile, MidiTrack, MetaMessage
-
-def write2csv(midi,txt):
-
-    # enter the information from the csv file to a text file
-    with open(txt, "w")as file:
-        for l in py_midicsv.midi_to_csv(midi):
-            file.writelines(l + "\n")
-
-# midi file to matrix encoder
 # fits input from midi files to numpy 3d arrays
+normalize_val = 100.0
 vocabulary_size=130 # 0-127 are different pitches, 128 note off and 129 for nothing
-class encoder:
+class fitting_input:
    def __init__(self, midi):
-       self.midi = midi # create a text file that corresponds to the midi file
-       self.csv=py_midicsv.midi_to_csv(midi)
-       print (self.csv)
-       self.tempo = int(self.csv[0].split(', ')[-1][:-1]) ### tempo calculation *******
+       self.csv = py_midicsv.midi_to_csv(midi) # create a text file that corresponds to the midi file
+   def get_csv(self):
+       # enter the information from the csv file to a text file
+       two=0
+       three=0
+       with open("c:/project/mid.txt", "w" )as file:
+           for l in self.csv:
+               if l[0]=='2':
+                print (l)
+                two+=1
+               if l[0]=='3':
+                three+=1
+               file.writelines(l+"\n")
+           print (three,two)
+   def midi2numpy(self):
+       float_lst=[float(line.replace("Note_on_c, ", "").split(", ")[3]) for line in self.csv if "Note_on_c" in line ] # remove unnecessary characters from the midi file
 
-   def midi2matrix(self):
-
-       note_events=[line for line in self.csv if "Note_" in line ] # remove unnecessary characters from the midi file
-
-       notes_flag=[1 if parameter.split(", ")[2]=="Note_on_c" else 0 for parameter in note_events]
-       durations=[int(parameter.split(", ")[1]) for parameter in note_events] # get note durations from note events
-       pitches=[int(parameter.split(", ")[4]) for parameter in note_events] # get the pitches from note events
-       velocities=[int(parameter.split(", ")[5][:-1]) for parameter in note_events] # get the velocities from note events
-       return input_utils.input2matrix(pitches, velocities, durations, notes_flag)
-
-
-# translates the given matrix/3d data type into an audible file
-class decoder:
-    def __init__(self, matrix, tempo):
-        self.matrix=matrix
-        self.tempo=tempo
-    def matrix2midi(self):
-        matrix=self.matrix
-        mid = MidiFile() # create a new midi file
-        mid.ticks_per_beat=self.tempo
-        track = MidiTrack() # build a new track
-        mid.tracks.append(track)
-        n_x, T_x=matrix.shape
-
-        prev_t=0
-        g=0
-        # handle the first note event
-        for pitch in range(len(matrix[:,0])):
-
-            velocity = int(matrix[pitch, 0])  # the current velocity of the pitch
-            if velocity!= 0:
-                track.append(Message('note_on', note=pitch, velocity=velocity, time=0))
-
-        for t in range(1, T_x):
-            # insignificant time stamps
-            if not np.any(matrix[:, t]):  # if the array is an array of zeros
-                continue
-            else:
-                count=0
-                flag=1
-                for pitch in range(len(matrix[:,t])):
-                    if matrix[pitch,t]==matrix[pitch, prev_t]:
-                        continue
-                    velocity = int(matrix[pitch, t])  # the current velocity of the pitch
-                    if count:
-                        flag=0
-                    if velocity != 0 :
-
-                        track.append(Message('note_on', note=pitch, velocity=velocity, time=flag*int(t-prev_t)))
-                        count+=1
-                    elif matrix[pitch, prev_t]!=0:
-                        track.append(Message('note_off', note=pitch, velocity=0, time=flag*int(t-prev_t)))
-                        count+=1
-                prev_t = t
-
-
-        for pitch in range(len(matrix[:, prev_t])):
-
-            velocity = int(matrix[pitch, prev_t])  # the current velocity of the pitch
-            if velocity != 0:
-                track.append(Message('note_off', note=pitch, velocity=0, time= int(T_x -1- prev_t)))
-        mid.save('c://project/music/new_song.mid')
-
-def main():
-
-    m = encoder("c://project/music/amazing.midi")#.midi2matrix()
-    matrix=m.midi2matrix()
-    m=decoder(matrix, m.tempo).matrix2midi()
-    write2csv("c://project/music/amazing.midi", "c:/project/mid.txt")
-    write2csv("c://project/music/new_song.mid", "c:/project/csv.txt")
-if __name__ == '__main__':
-    main()
+       input = np.array(float_lst) / normalize_val  # creating a numpy array of the frequencies+ normalization
+       return input
+m=fitting_input("c://project/music/prelude1.mid").get_csv()
