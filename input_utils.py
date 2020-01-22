58834d270d7f15e377d933a4ffc8699f90a376ac
gshachar12 <57983567+gshachar12@users.noreply.github.com>
2020-01-22 07:53:09 +0200

    Add files via upload

--- input_utils.py
+++ input_utils.py
@@ -1,15 +1,37 @@
-def one_hot(vocabulary_size, input_val):
-    """
-    :
-    build a one hot vector from the given input
 
-    :param:
-     vocabulary_size: the number of unique characters in the database
-     input_val: the index of the value in the list of unique characters
+""" data representation of the input- converts the musical input into a machine readable language"""
+
+import numpy as np
+normalize_val = 100.0
+
+
+""" parameters and initialization"""
+n_h = 90  # the number of hidden states
+
+def input2matrix(pitches, durations, velocities, notes_flag):
+    """
+
+    :param pitches:
+    :param velocities:
     :return:
     """
-    one_hot=np.zeros((vocabulary_size, 1)) # a new array of zeros
-    one_hot[input]=1
+    n_x=128 # number of pitches in a vector (there are 128 possible pitches in a midi file)
+    durations.sort() # sort the durations list
+    events_time=sorted(set(durations))
+    T= len(events_time)# the length of the input time sequences
+    #build a one hot vector from the given input
 
-    return one_hot
+    matrix=np.zeros((n_x, T))
+    # repetition count setting
+    repetition_count=np.array(events_time)
+    matrix[0,:] = repetition_count
+    notes_vector = np.zeros((n_x-1))  # a new array of zeros that will be used to keep track of the notes being played
+    for t in range(len(durations)):
+        pitch = pitches[t]  # the current pitch being played
+        velocity = velocities[t]  # the current velocity of the pitch
+        notes_vector[pitch-1] = velocity * notes_flag[t]
+
+        matrix[1:, events_time.index(durations[t])] = notes_vector
+
+    return matrix
 
